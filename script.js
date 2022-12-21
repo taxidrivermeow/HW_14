43f24892e88219f917b894f8fa0d19750b343be5
taxidrivermeow <fupsy1@gmail.com>
2022-12-21 14:16:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const palindromeInput = document.getElementById('palindrome_input');
+const palindromeResSpan = document.getElementById('palindrome_res');
+const reverseInput = document.getElementById('reverse_input');
+const reverseResultSpan = document.getElementById('reverse_result');
+const stringValueInput = document.getElementById('string_value');
+const characterValueInput = document.getElementById('character_value');
+const resCalcSpan = document.getElementById('res_calc');
+
+function checkPalindrome(){
+    // get value from input to str and then call isPalindrome(str)
+    palindromeResSpan.innerHTML = `<b>${isPalindrome(palindromeInput.value)}</b>`;
+}
+
+function isPalindrome(str){
+    str = str.toLowerCase();
+    const checkStr = reverser(str);
+    return (str === checkStr) ? true : false;
+}
+
+function reverseString(){
+    // get value from input to str and then call reverseString(str)
+    reverseResultSpan.innerHTML = `<b>${reverser(reverseInput.value)}</b>`;
+}
+
+function reverser(str){
+    return str.split('').reverse().join('');
+}
+
+function getCount(){
+    // get value from input to str and c and then call countChar(str,c)
+    resCalcSpan.innerHTML = `<b>${countChar(stringValueInput.value, characterValueInput.value)}</b>`;
+}
+
+function countChar(str,c){
+    return str.split(c).length - 1;
+}
